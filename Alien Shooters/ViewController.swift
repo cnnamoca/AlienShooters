036793f8e80cf9871e43c3eca185ffab9501ff0c
CarloNamoca <carlo.namoca@gmail.com>
2018-09-24 16:02:43 -0700

    Initial Commit

---
+++ Alien Shooters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alien Shooters
+//
+//  Created by Carlo Namoca on 2018-09-24.
+//  Copyright © 2018 Carlo Namoca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
