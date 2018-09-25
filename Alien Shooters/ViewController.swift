f499e45cbe675b26feb19f3068c3027028ff3679
CarloNamoca <carlo.namoca@gmail.com>
2018-09-25 12:39:40 -0700

    Add addEnemy function

--- Alien Shooters/ViewController.swift
+++ Alien Shooters/ViewController.swift
@@ -23,9 +23,21 @@ class ViewController: UIViewController {
     }
     
     // Gives you a random decimal in between firstNum and secondNum
+    // TODO: - Figure out new Swift random function
     func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
         return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
+    
+    func addEnemy() {
+        let enemyScene = SCNScene(named: "art.scnassets/alien.scn")
+        let alienNode = enemyScene?.rootNode.childNode(withName: "alien", recursively: false)
+        alienNode?.position = SCNVector3(randomNumbers(firstNum: -1, secondNum: 1),
+                                         randomNumbers(firstNum: -0.5, secondNum: 0.5) ,
+                                         randomNumbers(firstNum: -1, secondNum: 1))
+        self.sceneView.scene.rootNode.addChildNode(alienNode!)
+    }
+    
+    
 
 
 }
